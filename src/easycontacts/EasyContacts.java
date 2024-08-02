6cd1340d756f2b6dcbe5c016fd58fc7612ea7c07
oshada01 <oshadavitharana1@gmail.com>
2024-08-03 01:20:35 +0530

    Easy Contacts Java Project

--- src/easycontacts/EasyContacts.java
+++ src/easycontacts/EasyContacts.java
@@ -2,6 +2,22 @@
 package easycontacts;
 
 
+class Contact{
+    String name;
+    String phoneNumber;
+    String email;
+    String address;
+    String notes;
+    
+    Contact(String name, String phoneNumber, String email, String address, String notes){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.notes = notes;
+    }
+}
+
 public class EasyContacts {
 
     
